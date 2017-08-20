1fa2f0cfa8be104303533c53a83ae6e40789f232
AwayQuEM <awayquym@gmail.com>
2017-08-20 16:10:19 +0800

    temp

---
+++ src/Chart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Chart extends Component {
+  render() {
+    return (
+      <div className="Chart">
+        111
+      </div>
+    );
+  }
+}
+
+export default Chart;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Chart from './Chart';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((<div>
+  <App />
+  <Chart />
+</div>
+), document.getElementById('root'));
 registerServiceWorker();
